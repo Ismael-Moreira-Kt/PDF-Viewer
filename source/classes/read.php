a39b9a7fd882b3f166de7be81117e8422e2dc932
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-23 08:27:55 +0100

    feat: Editing read.php
    I have configured the headers of the pdf reader.

--- source/classes/read.php
+++ source/classes/read.php
@@ -1,6 +1,10 @@
 <?php 
     $file = '../assets/pdf/Java.pdf';
 
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: inline; filename="' . basename($file) . '"');
+    header('Content-Transfer-Encoding: binary');
+    header('Accept-Ranges: bytes');
 
-    
+
 ?>
